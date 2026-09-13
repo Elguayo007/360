8919ff415f13f96351925b4030253c453bacca66
Elguayo007 <javieraaron336@gmail.com>
2026-09-13 16:31:56 -0300

    Delete 360 directory

--- 360/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://gcxnihnzqlieslusnltm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_egOHaUG_vfACML7KfxtfcQ_GuBh3tSF";
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  
-);
